a4693e4f2b4b479a50e448eaa25e6bcbf5ac8f13
Kalln <karl.widkor@gmail.com>
2022-09-29 12:17:32 +0200

    add comments

--- biathlon1.py
+++ biathlon1.py
@@ -1,3 +1,6 @@
+# Vi importerar från biblioteket "random" funktionen randint(a, b) som
+# genererar ett slumpmässigt heltal mellan a och b.
+
 from random import randint
 
 def splash():
@@ -18,25 +21,32 @@ def open():
 def closed():
     return 1
 
+# is_open och is_closed tar ett argument: målet (target)
+# Den kontrollerar om målet är öppet eller stängt i resp. funktion
+# genom att kontrollera om den är lika med open() resp. closed()
 def is_open(target):
     if target == open():
         return True
     else: return False
     
 def is_closed(target):
-    if taget == closed():
+    if target == closed():
         return True
     else: return False
     
+# Genererar en ny lista med fem nya måltavlor som är öppna. 
 def new_targets():
     targets = []
     for _ in range(5):
         targets.append(open())
     return targets
     
+# Tar en tavla (targets) och ett index i den här tavlan (target).
+# close_target ändrar värdet på de valda indexet till closed()
 def close_target(target, targets):
     targets[target] = closed()
 
+# Räknar antal träffar genom att kontrollera vilka mål som är stängda. 
 def hits(targets):
     n = 0
     for x in targets:
@@ -44,6 +54,8 @@ def hits(targets):
             n = n + 1
     return n
 
+# tar ett mål (target) och omvandlar denna till ett tecken beroende på om den är
+# stängd eller öppen. Detta gör vi för att det ska vara mer visuellt för användaren.
 def target_to_string(target):
     if is_open(target):
         return "* "
@@ -67,25 +79,25 @@ def view_targets(targets):
     
     """)
 
+# 50/50 chans att vi träffar. Om det slumpmässiga talet blir 1
+# har vi träffat.
 def random_hit():
     if randint(0, 1) == 1:
         return True
     else: return False
     
-        
+# 1. Kontrollera om vi träffat genom random_hit().
+# 2. om random_hit() ger tillbaka True betyder det att skottet
+# har träffat. 
+# 3. Kontrollera om målet som vi vill skjuta på är stängt.
+# 4. Om ja, träff och vi skriver att det var en träff på öppet mål.
+# 5. Om nej, träff men vi skriver attt det var en träff på stängt mål.        
 def shoot(targets, target):
     if random_hit() == True:
-        if is_open(targets[target]) = True:
+        if is_open(targets[target]) == True:
             close_target(target, targets)
             return "hit on open target"
         else: return "hit on closed target"
     else: return "miss"
-    
-    
-    
-    
-    
-    
-    
     
     
